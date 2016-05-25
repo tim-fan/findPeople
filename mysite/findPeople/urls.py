aba3d0869df5126d5db07773706e43cb3b81a61a
Tim Fanselow <tim.fanselow@crown.com>
2016-05-25 12:37:42 +1200

    Added saving of user locations to DB

--- mysite/findPeople/urls.py
+++ mysite/findPeople/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.viewLocations, name='location view'),
-    url(r'^sendLocation/$', views.sendLocation, name='send location view'),
-    url(r'^userLocations/$', views.getLocations, name='get locations view'),
+    url(r'^locations/$', views.locations, name='get/set location data view'),
 ]
--- mysite/findPeople/views.py
+++ mysite/findPeople/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
@@ -9,33 +10,27 @@ from .models import GeoLocation
 def viewLocations(request):
     #return HttpResponse("Here are the locations of people")
     location = GeoLocation.objects.first()
-    return render(request, 'findPeople/locationPage.html', {'lat':location.lat, 'lon':location.lon})
+    return render_to_response('findPeople/locationPage.html')
+    #return render(request, 'findPeople/locationPage.html', {'lat':location.lat, 'lon':location.lon})
 
-def sendLocation(request):
-    # if this is a POST request we need to process the form data
+def locations(request):
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = GeoLocationForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
+        #POST goes here . is_ajax is must to capture ajax requests. Beginner's pit.
+        if request.is_ajax():
+            #Always use get on request.POST. Correct way of querying a QueryDict.
+            lat = request.POST.get('lat')
+            lon = request.POST.get('lon')
+            data = {"lat":lat , "lon" : lon}
             
-            #remove all old locations
-            GeoLocation.objects.all().delete()
+            #create and save a new geolocation with given coords.
+            g = GeoLocation(lat=lat, lon=lon)
+            g.save()
             
-            #save the new location
-            form.save()
-            # redirect to a new URL:
-            return HttpResponseRedirect('/findPeople/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = GeoLocationForm()
-
-    return render(request, 'findPeople/geoLocation.html', {'form': form})
-
-def getLocations(request):
-    # return know locations as json
+            #Returning same data back to browser
+            return JsonResponse(data)
+    
+    #Get goes here
+    # return known locations as json
     locationsDict = dict(locations=list(GeoLocation.objects.values()))
     return JsonResponse(locationsDict)
 
